e45fb92f76d6cb0918ad70ac6fdaa99692ae17d4
GalevM <martingalev8@gmail.com>
2026-03-23 20:03:39 +0100

    Add test migration and dotenv

---
+++ backend/src/main/java/finki/ukim/mk/backend/config/DotenvConfig.java
@@ -0,0 +1,15 @@
+package finki.ukim.mk.backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+}
