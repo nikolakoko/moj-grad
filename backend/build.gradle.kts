092438dec91b80a084201bf394712fb8a3f82bff
Nikola Jordanoski <nikolaj_koko@yahoo.com>
2026-04-18 19:53:34 +0200

    Handle validation exceptions.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,7 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // Database - versions managed by Spring Boot BOM
     runtimeOnly("org.postgresql:postgresql")
