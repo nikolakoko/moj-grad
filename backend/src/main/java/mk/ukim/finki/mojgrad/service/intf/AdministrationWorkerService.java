ff3019cf399e7502e5e5d2de0cdad42fef742fe5
GalevM <martingalev8@gmail.com>
2026-05-02 15:31:29 +0200

    Administrative worker grid

--- backend/src/main/java/mk/ukim/finki/mojgrad/service/intf/AdministrationWorkerService.java
+++ backend/src/main/java/mk/ukim/finki/mojgrad/service/intf/AdministrationWorkerService.java
@@ -1,5 +1,10 @@
 package mk.ukim.finki.mojgrad.service.intf;
 
+import mk.ukim.finki.mojgrad.dto.request.complaint.UserFilterRequest;
+import mk.ukim.finki.mojgrad.dto.response.complaint.AdministrationUserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface AdministrationWorkerService {
-    // TODO: define methods
+    Page<AdministrationUserResponse> findAdministrativeWorkers(UserFilterRequest filter, Pageable pageable);
 }
